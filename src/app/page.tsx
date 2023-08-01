984fe4481e325bf6fc65a445eba26064353571f4
HibaAqqaoui <h.aqqaoui@aui.ma>
2023-08-01 16:12:42 +0100

    showcase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
-import {prisma} from '@/lib/db'
+import { Button } from "@/components/ui/button";
+import { prisma } from '@/lib/db';
 
-export default function home ()
+export default function Home ()
 {
-    
+    return <Button>Hello World</Button>;
 }
